21ce3c0a8d34b108c70d08740be9edb40b0a410c
Ryan Ricard <ryan.ricard@gmail.com>
2014-02-13 15:56:53 -0600

    Ember pro tip to help people understand how application state is effected when transitioning within a resource route.

---
+++ transitions_within_resource_does_not_change_state_of_ancestor_controllers/js/app.js
@@ -0,0 +1,41 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource("dogs", function(){
+    this.route("new");
+  });
+});
+
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo("dogs");
+  }
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller, model){
+    controller.set("timestamp", (new Date()).getTime());
+  }
+});
+
+App.DogsRoute = Ember.Route.extend({
+  setupController: function(controller, model){
+    controller.set("timestamp", (new Date()).getTime());
+  }
+});
+
+App.DogsIndexRoute = Ember.Route.extend({
+  setupController: function(controller, model){
+    controller.set("timestamp", (new Date()).getTime());
+  }
+});
+
+App.DogsNewRoute = Ember.Route.extend({
+  setupController: function(controller, model){
+    controller.set("timestamp", (new Date()).getTime());
+  }
+});
+
+App.HeaderView = Ember.View.extend({
+  templateName: "header"
+})
