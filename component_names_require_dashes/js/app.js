b64dffd86792cc19c17cf5605494a94d55006441
Ryan Ricard <ryan.ricard@gmail.com>
2014-02-13 16:43:52 -0600

    Cleaned up unecessary JavaScript in component_names_require_dashes's app.js.

--- component_names_require_dashes/js/app.js
+++ component_names_require_dashes/js/app.js
@@ -1,17 +1,5 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
-
-// for more details see: http://emberjs.com/guides/components/
-
 App.IceHockey = Ember.Component.extend();
 
 App.Curling = Ember.Component.extend();
